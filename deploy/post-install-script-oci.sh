640731ae3c15c940bb2559ff72c33b50d84d08c7
Kenneth Heung <kenneth.heung@gmail.com>
2022-02-18 11:49:55 +0800

    file structure changed in this release - fixed path - since some step already done comment out the completed task

--- deploy/post-install-script-oci.sh
+++ deploy/post-install-script-oci.sh
@@ -264,7 +264,7 @@ create_other_categories(){
     printf "\n\n"
     echo -e "\e[0;32m${bold}Create other categories ${normal}"
     git clone https://github.com/project-sunbird/knowledge-platform.git -b ${knowledge_platform_tag}
-    cd knowledge-platform/definition-scripts
+    cd knowledge-platform/scripts/definition-scripts
     sed -i "s#{{host}}#http://${private_ingressgateway_ip}/taxonomy#g" *
     sed -i "s#curl#curl -sS#g" *
     while read -r line; do printf "\n\n" >> /tmp/all_category_create.sh && cat $line >> /tmp/all_category_create.sh; done <<< $(ls)
@@ -681,12 +681,17 @@ echo -e "\e[0;90m${bold}knowledge-platform-tag: $knowledge_platform_tag ${normal
 echo -e "\e[0;90m${bold}user password: $user_passwords ${normal}"
 printf "\n\n"
 
-cassandra_forms
+# there will be changes for the steps below
+#
+# if organization already created - fill in the ID here, otherwise, please comment it
+organisation="0134771038920867840"
+# just comment out the completed step if you need to re-run the script
+# cassandra_forms
 get_x_authenticated_token
-create_organisation
-create_master_categories
-create_default_licenses
-create_default_channel_license
+# create_organisation
+# create_master_categories
+# create_default_licenses
+# create_default_channel_license
 create_other_categories
 system_settings
 create_users
